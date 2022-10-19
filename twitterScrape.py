2cc9087dee7b240687874f54e109141ef33fae58
TeresaVu <95201928+TeresaVu@users.noreply.github.com>
2022-10-19 08:59:05 -0400

    separate lists of tweets between hashtags

--- twitterScrape.py
+++ twitterScrape.py
@@ -2,26 +2,27 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 
 # Created a list to append all tweet attributes(data)
-attributes_container = []
+mediaLitWeek = []
+digCitWeek = []
 
-print("#MediaLitWeek")
 # Using TwitterSearchScraper to scrape data and append tweets to list
 for i,tweet in enumerate(sntwitter.TwitterHashtagScraper('#MediaLitWeek').get_items()):
     if i>10:
         break
-    attributes_container.append([tweet.date.date(), tweet.likeCount, tweet.user.username, tweet.content])
-    
-# Creating a dataframe from the tweets list above 
-tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Like Count", "User", "Tweets"])
-print(tweets_df)
+    mediaLitWeek.append([tweet.date.date(), tweet.likeCount, tweet.user.username, tweet.content])
 
-print("#DigCitWeek")
-# Using TwitterSearchScraper to scrape data and append tweets to list
 for i,tweet in enumerate(sntwitter.TwitterHashtagScraper('#DigCitWeek').get_items()):
     if i>10:
         break
-    attributes_container.append([tweet.date.date(), tweet.likeCount, tweet.user.username, tweet.content])
+    digCitWeek.append([tweet.date.date(), tweet.likeCount, tweet.user.username, tweet.content])
     
 # Creating a dataframe from the tweets list above 
-tweets_df = pd.DataFrame(attributes_container, columns=["Date Created", "Like Count", "User", "Tweets"])
-print(tweets_df)
+mediaDF = pd.DataFrame(mediaLitWeek, columns=["Date Created", "Like Count", "User", "Tweets"])
+print(mediaDF)
+
+digDF = pd.DataFrame(digCitWeek, columns=["Date Created", "Like Count", "User", "Tweets"])
+print(digDF)
+
+# export the dataframe to a csv file if desired
+# mediaDF.to_csv("#MediaLitWeek.csv") 
+# digDF.to_csv("#DigCitWeek")
